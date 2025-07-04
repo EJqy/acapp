d4458cd4f710ef908d9455c09ee839892c748a6b
Qingyi Ji <jqy021018@gmail.com>
2025-07-05 07:47:20 +0800

    organizing all static

--- game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from game.views import *
-
-urlpatterns = [
-    path('', index, name = 'index'),
-	path('hidden', index2, name = 'index2')
-]
--- game/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-# Create your views here.
-
-def index(request):
-	return HttpResponse("一个史诗级的网页！")
-
-def index2(request):
-	return HttpResponse("Beautiful man-moth!!!")
