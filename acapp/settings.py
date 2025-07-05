3373a179978a81f38810f47aefcdfb8ddaa04674
Qingyi Ji <jqy021018@gmail.com>
2025-07-05 08:10:14 +0800

    Finally understand static files!

--- acapp/settings.py
+++ acapp/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['159.65.253.244']
 # Application definition
 
 INSTALLED_APPS = [
-	'game.apps.GameConfig',
+#`	'game.apps.GameConfig',
 	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
@@ -119,11 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'game/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'STATIC')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+	BASE_DIR / "game/static",
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
