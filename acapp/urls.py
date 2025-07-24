da866c7c418e4e112ac8810bff3f850fe99510a9
Qingyi Ji <jqy021018@gmail.com>
2025-07-25 05:28:35 +0800

    finish all the framework settings

--- acapp/urls.py
+++ acapp/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-#	path('game/', include('game.urls')),
+	path('game/', include('game.urls.index')),
 	path('admin/', admin.site.urls)
 ]
 
